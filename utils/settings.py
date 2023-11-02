8a9410df430e2507e0422dcabd608874cdfbc3f4
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2023-11-02 14:50:04 +0700

    Add database

--- utils/settings.py
+++ utils/settings.py
@@ -1,5 +1,12 @@
 from utils.config import ConfigManager
+from utils.database import Database
 import os
 
+# Set up config
 config_path = os.path.join(os.getcwd(), "config.yml")
 config_manager = ConfigManager(config_path)
+
+# Set up database
+db_info = config_manager.get("database")
+db = Database(db_info["host"], db_info["database"], db_info["user"], db_info["password"]).get()
+conversations = db["conversations"]
