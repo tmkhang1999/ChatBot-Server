5ee733e995da83d3770ba1b13353d6471edc7774
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2023-11-13 09:56:55 +0700

    Add /healthcheck to API routers

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from modules.routers import chat_router
 from utils.settings import config_manager
+from starlette.responses import JSONResponse
 
 app = FastAPI()
 app.include_router(chat_router, tags=["chat"])
+
+
+@app.get("/healthcheck")
+async def healthcheck():
+    try:
+        # If all checks pass, return a healthy response
+        return JSONResponse(status_code=200,
+                            content={"status": "OK"})
+
+    except Exception as e:
+        # If any checks fail, return an unhealthy response with details
+        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
     app_info = config_manager.get("app")
