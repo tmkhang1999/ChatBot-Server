494eed070e29da082f8e587eb1f7d8468884dd48
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2023-11-24 12:13:26 +0700

    Update models.py
    Add TrialRequest and AddUserRequest

--- modules/models.py
+++ modules/models.py
@@ -1,7 +1,23 @@
+from typing import Union
 from pydantic import BaseModel
 
 
-class ChatRequest(BaseModel):
-    """Request body for streaming."""
-    conversation_id: str
+class TrialRequest(BaseModel):
+    """Request body for trial streaming before login."""
+    conversation_id: Union[None, str]
     message: str
+
+
+class AddUserRequest(BaseModel):
+    """Request body for adding user. Conversation_id is not None if the user try before login"""
+    user_id: str
+    conversation_id: Union[None, str]
+
+
+class ChatRequest(BaseModel):
+    """Request body for chat streaming after login."""
+    user_id: str
+    conversation_id: Union[None, str]
+    conversation_name: Union[None, str]
+    message: str
+
