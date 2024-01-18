b64fdb01447f796b10610ea2b7a10ee6d427640c
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2024-01-18 15:56:41 +0700

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,7 +1,9 @@
+import json
 import logging
 import uuid
 
 import dataset
+from fastapi import HTTPException
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 
@@ -12,23 +14,13 @@ class Database:
     def __init__(self, host, database, user, password) -> None:
         """
         Initialize the Database object with connection details.
-
-        Args:
-            host (str): The database host.
-            database (str): The database name.
-            user (str): The database user.
-            password (str): The database password.
         """
         # Check if all required database variables are provided
         if not all([host, database, user, password]):
             log.error("One or more database variables are missing, exiting...")
             raise SystemExit
 
-        self.url = f"mysql://{user}:{password}@{host}/{database}"
-
-        self.db = self.get()
-        self.conversations_table = self.db["conversations"]
-        self.users_table = self.db["users"]
+        self.url = f"mysql+pymysql://{user}:{password}@{host}/{database}"
 
         # Initialize tables
         self._setup_tables()
@@ -54,24 +46,30 @@ class Database:
         if not database_exists(engine.url):
             create_database(engine.url)
 
+        db = self.get()
+
         # Create 'users' table if missing
-        if "users" not in self.db:
-            users = self.db.create_table("users")
-            users.create_column("user_id", self.db.types.text)
-            users.create_column("conversation_ids", self.db.types.json)
+        if "users" not in db:
+            users = db.create_table("users")
+            users.create_column("user_id", db.types.text)
+            users.create_column("conversation_ids", db.types.json)
             log.info("Created missing table: users")
+            print("Created missing table: users")
 
         # Create 'conversations' table if missing
-        if "conversations" not in self.db:
-            conversations = self.db.create_table("conversations")
-            conversations.create_column("conversation_id", self.db.types.text)
-            conversations.create_column("name", self.db.types.text)
-            conversations.create_column("history", self.db.types.json)
+        if "conversations" not in db:
+            conversations = db.create_table("conversations")
+            conversations.create_column("conversation_id", db.types.text)
+            conversations.create_column("name", db.types.text)
+            conversations.create_column("history", db.types.json)
             log.info("Created missing table: conversations")
+            print("Created missing table: conversations")
 
         # Commit the changes to the database and close the connection
-        self.db.commit()
-        self.db.close()
+        db.commit()
+        db.close()
+
+        print("Successfully set up the tables")
 
     def add_user(self, user_id, conversation_ids):
         """
@@ -81,17 +79,21 @@ class Database:
             user_id (str): The user ID.
             conversation_ids (list): List of conversation IDs associated with the user.
         """
+        db = self.get()
+        users_table = db["users"]
+
         # Check if the user already exists
-        if self.users_table.find_one(user_id=user_id):
-            log.warning(f"User with ID {user_id} already exists. Skipping insertion.")
-            return
+        if users_table.find_one(user_id=user_id):
+            db.close()
+            raise HTTPException(status_code=409,
+                                detail=f"User with ID {user_id} already exists. Skipping insertion.")
 
         # Insert new user
-        self.users_table.insert(dict(user_id=user_id, conversation_ids=conversation_ids))
+        users_table.insert(dict(user_id=user_id, conversation_ids=json.dumps(conversation_ids)))
         log.info(f"Added user with ID {user_id} to the 'users' table.")
 
-        self.db.commit()
-        self.db.close()
+        db.commit()
+        db.close()
 
     def add_conversation(self, user_id, conversation_id, name):
         """
@@ -102,37 +104,41 @@ class Database:
             conversation_id (str): The unique conversation ID.
             name (str): The name of the conversation.
         """
+        db = self.get()
+        users_table = db["users"]
+        conversations_table = db["conversations"]
+
         # Check if the user exists
-        user_record = self.users_table.find_one(user_id=user_id)
+        user_record = users_table.find_one(user_id=user_id)
         if not user_record:
-            log.warning(f"User with ID {user_id} not found. Cannot add conversation.")
-            self.db.close()
-            return
+            db.close()
+            raise HTTPException(status_code=404,
+                                detail=f"User with ID {user_id} not found.")
 
         # Check if the conversation ID already exists
-        if self.conversations_table.find_one(conversation_id=conversation_id):
-            log.warning(f"Conversation with ID {conversation_id} already exists. Skipping insertion.")
-            self.db.close()
-            return
+        if conversations_table.find_one(conversation_id=conversation_id):
+            db.close()
+            raise HTTPException(status_code=409,
+                                detail=f"Conversation with ID {conversation_id} already exists.")
 
         # Insert the new conversation
-        self.conversations_table.insert(
-            dict(conversation_id=conversation_id, name=name, history={})
+        conversations_table.insert(
+            dict(conversation_id=conversation_id, name=name, history=json.dumps(dict()))
         )
 
         # Update the user's conversation IDs
-        user_conversation_ids = user_record["conversation_ids"]
+        user_conversation_ids = json.loads(user_record["conversation_ids"])
         user_conversation_ids.append(conversation_id)
-        self.users_table.update(
-            dict(user_id=user_id, conversation_ids=user_conversation_ids),
+        users_table.update(
+            dict(user_id=user_id, conversation_ids=json.dumps(user_conversation_ids)),
             ['user_id']
         )
 
         log.info(
             f"Added conversation with ID {conversation_id} to the 'conversations' table and linked it to user {user_id}.")
 
-        self.db.commit()
-        self.db.close()
+        db.commit()
+        db.close()
 
     def get_user_conversations(self, user_id):
         """
@@ -144,22 +150,25 @@ class Database:
         Returns:
             dict: Dictionary of user's conversations {conversation_id: name}.
         """
-        user_record = self.users_table.find_one(user_id=user_id)
+        db = self.get()
+        users_table = db["users"]
+        conversations_table = db["conversations"]
 
+        user_record = users_table.find_one(user_id=user_id)
         if user_record:
-            user_conversation_ids = user_record["conversation_ids"]
+            user_conversation_ids = json.loads(user_record["conversation_ids"])
             user_conversations = {}
 
             for conversation_id in user_conversation_ids:
-                conversation_record = self.conversations_table.find_one(conversation_id=conversation_id)
+                conversation_record = conversations_table.find_one(conversation_id=conversation_id)
                 if conversation_record:
                     user_conversations[conversation_id] = conversation_record["name"]
 
-            self.db.close()
+            db.close()
             return user_conversations
 
         log.warning(f"User with ID {user_id} not found.")
-        self.db.close()
+        db.close()
         return None
 
     def get_history(self, conversation_id):
@@ -172,15 +181,17 @@ class Database:
         Returns:
             dict or None: The saved history for the conversation or None if not found.
         """
-        conversation_record = self.conversations_table.find_one(conversation_id=conversation_id)
+        db = self.get()
+        conversations_table = db["conversations"]
 
+        conversation_record = conversations_table.find_one(conversation_id=conversation_id)
         if conversation_record:
             history = conversation_record["history"]
-            self.db.close()
+            db.close()
             return history
 
         log.warning(f"Conversation with ID {conversation_id} not found.")
-        self.db.close()
+        db.close()
         return None
 
     def update_history(self, conversation_id, new_history):
@@ -191,19 +202,22 @@ class Database:
             conversation_id (str): The conversation ID.
             new_history (dict): The new history to be updated for the conversation.
         """
+        db = self.get()
+        conversations_table = db["conversations"]
+
         # Check if the conversation exists
-        existing_record = self.conversations_table.find_one(conversation_id=conversation_id)
+        existing_record = conversations_table.find_one(conversation_id=conversation_id)
 
         if existing_record:
             # Update the history for the conversation
-            self.conversations_table.update(
+            conversations_table.update(
                 dict(conversation_id=conversation_id, history=new_history),
                 ['conversation_id']
             )
             log.info(f"Updated history for conversation with ID {conversation_id}.")
 
-        self.db.commit()
-        self.db.close()
+        db.commit()
+        db.close()
 
     def generate_unique_conversation_id(self, temporary_ids):
         """
