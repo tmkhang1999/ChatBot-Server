04b3a9aed5ad09a721b316a85e3fc9ad028baeda
Khang Tran <74235084+tmkhang1999@users.noreply.github.com>
2024-09-18 16:16:46 +0200

    Reorganize the budget_category examples

---
+++ modules/examples/budget_category/en.py
@@ -0,0 +1,30 @@
+english_examples = [
+    {
+        "input": "[\"Venue scouting and selection\",  \"Finalize venue booking\", \"Hire Wedding Planner and Vendors\", "
+                 "\"Guest list preparation\", \"Live music booking\", \"Catering arrangement\", \"Decor and theme design\", "
+                 "\"Equipment rental (chairs, tables, etc.)\", \"Transport for Guests (Limited or local)\", "
+                 "\"Vendor finalizations\", \"Event timeline and schedule\", \"Wedding attire and accessories\", "
+                 "\"Photography and videography\", \"Permits and licenses\", \"Weather contingency planning\", "
+                 "\"Budget review and adjustments\", \"Vendor confirmation\"]",
+        "answer": "{{"
+                  "\"0\": \"Venue\", "
+                  "\"1\": \"Venue\", "
+                  "\"2\": \"Staffing\", "
+                  "\"3\": \"Others\", "
+                  "\"4\": \"Entertainment\", "
+                  "\"5\": \"Food and Drinks\", "
+                  "\"6\": \"Decorations and Ambiance\", "
+                  "\"7\": \"Technology\", "
+                  "\"8\": \"Transportation\", "
+                  "\"9\": \"Staffing\", "
+                  "\"10\": \"Others\", "
+                  "\"11\": \"Others\", "
+                  "\"12\": \"Technology\", "
+                  "\"13\": \"Others\", "
+                  "\"14\": \"Others\", "
+                  "\"15\": \"Others\", "
+                  "\"16\": \"Staffing\""
+                  "}}"
+
+    }
+]
