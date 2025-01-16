26ace96c41a5cf697d3c3f78191e45355d7753cd
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2025-01-16 01:04:09 +0100

    Update budget_categorizer.py

--- modules/services/tools/budget_category/budget_categorizer.py
+++ modules/services/tools/budget_category/budget_categorizer.py
@@ -7,24 +7,19 @@ from modules.services.common import create_few_shot_prompt_template, create_exam
 from .examples import budget_category_examples
 from .prompt import BUDGET_CATEGORY_PROMPT
 
-BUDGET_CATEGORY_PROMPT_TEMPLATE = create_few_shot_prompt_template(
-    create_example_selector(budget_category_examples, OpenAIEmbeddings, FAISS, 2, ["input"]),
-    "User: {input}\nAI: {answer}",
-    BUDGET_CATEGORY_PROMPT,
-    "User: {input}\nAI: ",
-    ["input"]
-)
-
 
 class BudgetCategorizer:
     def __init__(self, MODEL_NAME, TEMPERATURE):
         self.llm = ChatOpenAI(model_name=MODEL_NAME, temperature=TEMPERATURE)
-        self.chain = LLMChain(
-            prompt=BUDGET_CATEGORY_PROMPT_TEMPLATE,
-            llm=self.llm,
-            verbose=True
+        self.BUDGET_CATEGORY_PROMPT_TEMPLATE = create_few_shot_prompt_template(
+            create_example_selector(budget_category_examples, OpenAIEmbeddings, FAISS, 2, ["input"]),
+            "User: {input}\nAI: {answer}",
+            BUDGET_CATEGORY_PROMPT,
+            "User: {input}\nAI: ",
+            ["input"]
         )
+        self.chain = self.BUDGET_CATEGORY_PROMPT_TEMPLATE | self.llm
 
     def execute(self, user_input: str) -> str:
         ans = self.chain.invoke({"input": user_input})
-        return ans["text"]
+        return ans.content
