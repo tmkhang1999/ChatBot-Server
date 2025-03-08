25ec7b5624106efe75504170402e7d2bcdac41a3
Khang Tran <74235084+tmkhang1999@users.noreply.github.com>
2025-03-08 17:43:01 +0100

    Update the database connection to READ_COMITTED

--- utils/settings.py
+++ utils/settings.py
@@ -11,5 +11,5 @@ config_manager = ConfigManager(config_path)
 planz_db_info = config_manager.get("planz_db")
 encoded_password = urllib.parse.quote_plus(planz_db_info["password"])
 url = f"mysql+pymysql://{planz_db_info['user']}:{encoded_password}@{planz_db_info['host']}:{planz_db_info['port']}/{planz_db_info['database']}"
-planz_db = SQLDatabase.from_uri(url, include_tables=planz_db_info["tables"])
+planz_db = SQLDatabase.from_uri(url, include_tables=planz_db_info["tables"], engine_args={"isolation_level": "READ_COMMITTED"})
 print("Successfully connect to PlanZ database")
